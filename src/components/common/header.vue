d573e6935b392ca94d78442e2c183721dcec7e6a
chenxiaojun <1102820661@qq.com>
2018-11-12 16:34:37 +0800

    添加公共头部

---
+++ src/components/common/header.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <div class="room_detail_header">
+    <div class="room_detail_header_left">
+      <i class="iconfont" @click.stop="goBack">&#xe603;</i>
+    </div>
+    <div class="room_detail_header_title">
+      {{label}}
+    </div>
+    <div class="room_detail_header_right">
+      <slot name="header_right" />
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class RoomDetailHeader extends Vue {
+  @Prop({default: ''}) private label!: string;
+
+  private goBack(): void {
+    if (window.history.length <= 1) {
+      this.$router.push({path: '/'});
+    } else {
+      this.$router.back();
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.share_cion {
+  color: #5B5B5B;
+  font-size: 20px;
+}
+.room_detail_header {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 12px 15px;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #EBEBEB;
+}
+.room_detail_header_left {
+  width: 20%;
+}
+.room_detail_header_title {
+  font-size: 18px;
+  color: #333;
+  font-weight: 500;
+}
+.room_detail_header_right {
+  width: 20%;
+  color: #999999;
+  display: flex;
+  font-size: 16px;
+  justify-content: space-between;
+}
+</style>
