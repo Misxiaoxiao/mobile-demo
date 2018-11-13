1d7e31a99211b5165028df1ba793fcd6d866a05a
chenxiaojun <1102820661@qq.com>
2018-11-13 13:49:05 +0800

    添加loading组件

---
+++ src/components/common/loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loading-spin">
+    <img :src="loading" alt="加载中" height="28" />
+  </div>  
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Loading extends Vue {
+  private loading: any = require('loading-svg/loading-spin.svg');
+}
+</script>
+
+<style lang="less">
+.loading-spin {
+  margin: 20px;
+  margin-top: 100px;
+  text-align: center;
+}
+</style>
