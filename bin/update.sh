3b92d751f8880e1fd4991cafbce2c38e7c79b6f1
chenxiaojun <1102820661@qq.com>
2018-11-14 17:26:21 +0800

    11.14

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,4 @@
 git add *
 git commit -am $1
 git push
-git subtree pull --prefix=src/vuex http://git.zuker.im/components/vuex-client.git master --allow-unrelated-histories
+git subtree pull --allow-unrelated-histories --prefix=src/vuex http://git.zuker.im/components/vuex-client.git master
