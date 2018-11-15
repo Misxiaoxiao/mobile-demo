93feeee40760c97d9be2bd3e6073304b8c9d0354
chenxiaojun <1102820661@qq.com>
2018-11-15 18:37:07 +0800

    updateInterface

--- bin/update.sh
+++ bin/update.sh
@@ -2,3 +2,4 @@ git add *
 git commit -am $1
 git push
 git subtree pull --squash --prefix=src/vuex http://git.zuker.im/components/vuex-client.git master
+git subtree pull --squash --prefix=src/interface http://git.zuker.im/components/interface.git master
